d9b75c08b2bd32b0b209582a541ae22359bf9fda
Giosuè Marinò <giosumarin@yahoo.com>
2021-12-15 17:14:33 +0100

    added cifar100 dataset and original net (vgg19)

--- experiments/performance_eval/VGG19/datasets.py
+++ experiments/performance_eval/VGG19/datasets.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist, cifar10
+from tensorflow.keras.datasets import mnist, cifar10, cifar100
+import numpy as np
 
 def MNIST(batch_size):
     ((x_train, y_train), (x_test, y_test)) = mnist.load_data()
@@ -32,6 +33,30 @@ def CIFAR10(batch_size):
     x_test[:,:,:,0] = (x_test[:,:,:,0]-123.680)
     x_test[:,:,:,1] = (x_test[:,:,:,1]-116.779)
     x_test[:,:,:,2] = (x_test[:,:,:,2]-103.939)
+
+    dataset = tf.data.Dataset.from_tensor_slices(
+  (tf.cast(x_train, tf.float32),
+   tf.cast(y_train,tf.int64)))
+    dataset = dataset.shuffle(1000).batch(batch_size)
+
+    return dataset, x_train, y_train, x_test, y_test
+
+def CIFAR100(batch_size):
+    num_classes = 100
+    (x_train, y_train), (x_test, y_test) = cifar100.load_data()
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+
+    mean = np.mean(x_train,axis=(0,1,2,3))
+    std = np.std(x_train, axis=(0, 1, 2, 3))
+    x_train = (x_train-mean)/(std+1e-7)
+    x_test = (x_test-mean)/(std+1e-7)
+
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+    y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
 
     dataset = tf.data.Dataset.from_tensor_slices(
   (tf.cast(x_train, tf.float32),
---
+++ experiments/performance_eval/VGG19/vgg19-cifar100.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+import numpy as np
+import keras
+#from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, AveragePooling2D
+from tensorflow.keras.initializers import he_normal
+from tensorflow.keras import optimizers
+from tensorflow.keras.callbacks import LearningRateScheduler, TensorBoard
+from tensorflow.keras.layers import BatchNormalization
+from keras.utils.data_utils import get_file
+from datasets import CIFAR100
+
+
+num_classes  = 10
+batch_size   = 128
+epochs       = 200
+iterations   = 391
+dropout      = 0.5
+weight_decay = 0.0001
+log_filepath = r'./vgg19_retrain_logs/'
+
+from keras import backend as K
+
+exec(open("../GPU.py").read())
+
+def scheduler(epoch):
+    if epoch < 80:
+        return 0.1
+    if epoch < 160:
+        return 0.01
+    return 0.001
+
+WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg19_weights_tf_dim_ordering_tf_kernels.h5'
+filepath = get_file('vgg19_weights_tf_dim_ordering_tf_kernels.h5', WEIGHTS_PATH, cache_subdir='models')
+
+# data loading
+dataset, x_train, y_train, x_test, y_test = CIFAR100(batch_size)
+
+# build model
+model = Sequential()
+
+# Block 1
+model.add(Conv2D(64, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block1_conv1', input_shape=x_train.shape[1:]))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block1_conv2'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+
+# Block 2
+model.add(Conv2D(128, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block2_conv1'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block2_conv2'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+
+# Block 3
+model.add(Conv2D(256, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block3_conv1'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(256, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block3_conv2'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(256, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block3_conv3'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(256, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block3_conv4'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
+
+# Block 4
+model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block4_conv1'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block4_conv2'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block4_conv3'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block4_conv4'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
+
+# Block 5
+model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block5_conv1'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block5_conv2'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block5_conv3'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='block5_conv4'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool'))
+
+# model modification for cifar-10
+model.add(Flatten(name='flatten'))
+model.add(Dense(4096, use_bias = True, kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='fc_cifa10'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(dropout))
+model.add(Dense(4096, kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='fc2'))  
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(dropout))      
+model.add(Dense(100, kernel_regularizer=keras.regularizers.l2(weight_decay), kernel_initializer=he_normal(), name='predictions_cifa10'))        
+model.add(BatchNormalization())
+model.add(Activation('softmax'))
+
+# load pretrained weight from VGG19 by name      
+model.load_weights(filepath, by_name=True)
+
+# -------- optimizer setting -------- #
+sgd = optimizers.SGD(lr=.1, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+tb_cb = TensorBoard(log_dir=log_filepath, histogram_freq=0)
+change_lr = LearningRateScheduler(scheduler)
+cbks = [change_lr,tb_cb]
+
+model.fit_generator(dataset, epochs=epochs,
+                    validation_data=(x_test, y_test))
+model.save('retrain.h5')
