8df8f278ac9cf5431d32228269f9d99337d0b1c4
Giosuè Marinò <giosumarin@yahoo.com>
2021-12-16 10:38:02 +0100

    augmentation + lr schedule

--- experiments/performance_eval/VGG19/vgg19-cifar100.py
+++ experiments/performance_eval/VGG19/vgg19-cifar100.py
@@ -11,9 +11,10 @@ from tensorflow.keras.callbacks import LearningRateScheduler, TensorBoard
 from tensorflow.keras.layers import BatchNormalization
 from keras.utils.data_utils import get_file
 from datasets import CIFAR100
+from keras.preprocessing.image import ImageDataGenerator
 
 
-num_classes  = 10
+num_classes  = 100
 batch_size   = 128
 epochs       = 200
 iterations   = 391
@@ -131,4 +132,45 @@ cbks = [change_lr,tb_cb]
 
 model.fit_generator(dataset, epochs=epochs,
                     validation_data=(x_test, y_test))
+
+batch_size = 128
+maxepoches = 500
+learning_rate = 0.1
+lr_decay = 1e-6
+lr_drop = 20
+
+def lr_scheduler(epoch):
+    return learning_rate * (0.5 ** (epoch // lr_drop))
+reduce_lr = keras.callbacks.LearningRateScheduler(lr_scheduler)
+
+#data augmentation
+datagen = ImageDataGenerator(
+    featurewise_center=False,  # set input mean to 0 over the dataset
+    samplewise_center=False,  # set each sample mean to 0
+    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=False,  # apply ZCA whitening
+    rotation_range=15,  # randomly rotate images in the range (degrees, 0 to 180)
+    width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+    height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+    horizontal_flip=True,  # randomly flip images
+    vertical_flip=False)  # randomly flip images
+# (std, mean, and principal components if ZCA whitening is applied).
+datagen.fit(x_train)
+
+
+
+#optimization details
+sgd = optimizers.SGD(lr=learning_rate, decay=lr_decay, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics=['accuracy'])
+
+# training process in a for loop with learning rate drop every 25 epoches.
+
+historytemp = model.fit_generator(datagen.flow(x_train, y_train,
+                                    batch_size=batch_size),
+                    steps_per_epoch=x_train.shape[0] // batch_size,
+                    epochs=maxepoches,
+                    validation_data=(x_test, y_test),callbacks=[reduce_lr],verbose=2)
+
+
 model.save('retrain.h5')
