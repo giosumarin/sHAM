17f2be590e30a969f9d257ce9a4c82c392319809
Giosuè Marinò <giosumarin@yahoo.com>
2022-01-13 17:00:35 +0100

    miss /

--- experiments/time_space/runner_rata.sh
+++ experiments/time_space/runner_rata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python uws_testing_space.py -t all -d /home/gioscatmarino/exp_balrog/pruPWS \
+python uws_testing_space.py -t all -d /home/gioscatmarino/exp_balrog/pruPWS/ \
 -m /home/gioscatmarino/exp_balrog/original_nets/vgg19cifar100.h5 -s cifar100 -q 0
 
-python uws_testing_space.py -t all -d /home/gioscatmarino/exp_balrog/pruCWS \
+python uws_testing_space.py -t all -d /home/gioscatmarino/exp_balrog/pruCWS/ \
 -m /home/gioscatmarino/exp_balrog/original_nets/vgg19cifar100.h5 -s cifar100 -q 0
