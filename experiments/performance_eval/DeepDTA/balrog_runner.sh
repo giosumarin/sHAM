76a59ab2cf3e99f0882eef77054afa7e11c38d79
Giosuè Marinò <giosumarin@yahoo.com>
2022-01-15 13:38:26 +0100

    upd

--- experiments/performance_eval/DeepDTA/balrog_runner.sh
+++ experiments/performance_eval/DeepDTA/balrog_runner.sh
@@ -1,16 +1,16 @@
 for k in 32 64 128 256
 do
-	python compression_only_quant_conv.py --compression uCWS --net original_nets/deepDTA_kiba.h5 --dataset KIBA --clusterfc 0 --clustercnn $k --prfc 0
+	python compression.py --compression uCWS --net original_nets/deepDTA_kiba.h5 --dataset KIBA --clusterfc 0 --clustercnn $k --prfc 0
 done
 
 for k in 32 64 128 256
 do
-	python compression_only_quant_conv.py --compression uPWS --net original_nets/deepDTA_kiba.h5 --dataset KIBA --clusterfc 0 --clustercnn $k --prfc 0
+	python compression.py --compression uPWS --net original_nets/deepDTA_kiba.h5 --dataset KIBA --clusterfc 0 --clustercnn $k --prfc 0
 done
 
 for k in 32 64 128 256
 do
-	python compression_only_quant_conv.py --compression uUQ --net original_nets/deepDTA_kiba.h5 --dataset KIBA --clusterfc 0 --clustercnn $k --prfc 0
+	python compression.py --compression uUQ --net original_nets/deepDTA_kiba.h5 --dataset KIBA --clusterfc 0 --clustercnn $k --prfc 0
 done
 
 
@@ -18,15 +18,15 @@ done
 
 for k in 32 64 128 256
 do
-	python compression_only_quant_conv.py --compression uCWS --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 0 --clustercnn $k --prfc 0
+	python compression.py --compression uCWS --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 0 --clustercnn $k --prfc 0
 done
 
 for k in 32 64 128 256
 do
-	python compression_only_quant_conv.py --compression uPWS --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 0 --clustercnn $k --prfc 0
+	python compression.py --compression uPWS --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 0 --clustercnn $k --prfc 0
 done
 
 for k in 32 64 128 256
 do
-	python compression_only_quant_conv.py --compression uUQ --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 0 --clustercnn $k --prfc 0
+	python compression.py --compression uUQ --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 0 --clustercnn $k --prfc 0
 done
