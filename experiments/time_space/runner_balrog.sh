90a508094193fc24a8426ce4f234b549acc94da0
Giosuè Marinò <giosumarin@yahoo.com>
2022-01-23 13:14:16 +0100

    upd

--- experiments/time_space/runner_balrog.sh
+++ experiments/time_space/runner_balrog.sh
@@ -100,67 +100,65 @@
 
 
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uCWS/ --keep 8 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uCWS/ --keep 8 --sr 0.5 --rr 0.5
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uCWS/ --keep 16 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uCWS/ --keep 16 --sr 0.5 --rr 0.5
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uCWS/ --keep 32 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uCWS/ --keep 32 --sr 0.5 --rr 0.5
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uCWS/ --keep 64 --sr 0.5 --rr 0.5
-
-
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uPWS/ --keep 8 --sr 0.5 --rr 0.5
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uPWS/ --keep 16 --sr 0.5 --rr 0.5
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uPWS/ --keep 32 --sr 0.5 --rr 0.5
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uPWS/ --keep 64 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uCWS/ --keep 64 --sr 0.5 --rr 0.5
 
 
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uUQ/ --keep 8 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uPWS/ --keep 8 --sr 0.5 --rr 0.5
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uUQ/ --keep 16 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uPWS/ --keep 16 --sr 0.5 --rr 0.5
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uUQ/ --keep 32 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uPWS/ --keep 32 --sr 0.5 --rr 0.5
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uUQ/ --keep 64 --sr 0.5 --rr 0.5
-
-
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uPWS/ --keep 64 --sr 0.5 --rr 0.5
 
 
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uCWS/ --keep 8 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uUQ/ --keep 8 --sr 0.5 --rr 0.5
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uCWS/ --keep 16 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uUQ/ --keep 16 --sr 0.5 --rr 0.5
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uCWS/ --keep 32 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uUQ/ --keep 32 --sr 0.5 --rr 0.5
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uCWS/ --keep 64 --sr 0.5 --rr 0.5
-
-
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uPWS/ --keep 8 --sr 0.5 --rr 0.5
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uPWS/ --keep 16 --sr 0.5 --rr 0.5
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uPWS/ --keep 32 --sr 0.5 --rr 0.5
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uPWS/ --keep 64 --sr 0.5 --rr 0.5
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 --dataset davis --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_davis/uUQ/ --keep 64 --sr 0.5 --rr 0.5
 
 
 
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uUQ/ --keep 8 --sr 0.5 --rr 0.5
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uUQ/ --keep 16 --sr 0.5 --rr 0.5
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uUQ/ --keep 32 --sr 0.5 --rr 0.5
-
-python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uUQ/ --keep 64 --sr 0.5 --rr 0.5
 
 
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uCWS/ --keep 8 --sr 0.5 --rr 0.5
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uCWS/ --keep 16 --sr 0.5 --rr 0.5
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uCWS/ --keep 32 --sr 0.5 --rr 0.5
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uCWS/ --keep 64 --sr 0.5 --rr 0.5
+
+
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uPWS/ --keep 8 --sr 0.5 --rr 0.5
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uPWS/ --keep 16 --sr 0.5 --rr 0.5
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uPWS/ --keep 32 --sr 0.5 --rr 0.5
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uPWS/ --keep 64 --sr 0.5 --rr 0.5
+
+
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uUQ/ --keep 8 --sr 0.5 --rr 0.5
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uUQ/ --keep 16 --sr 0.5 --rr 0.5
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uUQ/ --keep 32 --sr 0.5 --rr 0.5
+
+# python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 --dataset kiba --directory /mnt/sHAM/experiments/performance_eval/DeepDTA/deepDTA_kiba/uUQ/ --keep 64 --sr 0.5 --rr 0.5
 
 
 
@@ -169,6 +167,8 @@ python uws_slrf_space.py --compression also_quant --net /mnt/sHAM/experiments/pe
 
 
 
-python uws_testing_space.py -t all -d /mnt/sHAM/experiments/performance_eval/VGG19/vgg19cifar100/pruPWS/ -m /mnt/sHAM/experiments/performance_eval/VGG19/original_nets/vgg19cifar100.h5 -s cifar100 -q 0
+
+#DAFARE
+# python uws_testing_space.py -t all -d /mnt/sHAM/experiments/performance_eval/VGG19/vgg19cifar100/pruPWS/ -m /mnt/sHAM/experiments/performance_eval/VGG19/original_nets/vgg19cifar100.h5 -s cifar100 -q 0
 
 python uws_testing_space.py -t all -d /mnt/sHAM/experiments/performance_eval/VGG19/vgg19cifar100/pruUQ/ -m /mnt/sHAM/experiments/performance_eval/VGG19/original_nets/vgg19cifar100.h5 -s cifar100 -q 0
