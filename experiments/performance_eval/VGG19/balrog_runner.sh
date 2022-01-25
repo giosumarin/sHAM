f7a95920c8fde6115a29cdfaabb7e8d7a43b0050
Giosuè Marinò <giosumarin@yahoo.com>
2022-01-25 16:42:16 +0100

    upd

--- experiments/performance_eval/VGG19/balrog_runner.sh
+++ experiments/performance_eval/VGG19/balrog_runner.sh
@@ -37,18 +37,38 @@
 
 
 
-for p in 30 40 50 60 70 80 90 95 96 97 98 99
-do
-	for k in 2 16 32 64 128 256
-	do
-		python compression.py --compression pruPWS --net original_nets/vgg19cifar100.h5 --dataset CIFAR100 --clusterfc $k --clustercnn 0 --prfc $p
-	done
-done
 
-# for p in 30 40 50 60 70 80 90 95 96 97 98 99
-# do
-# 	for k in 2 16 32 64 128 256
-# 	do
-# 		python compression.py --compression pruUQ --net original_nets/vgg19cifar100.h5 --dataset CIFAR100 --clusterfc $k --clustercnn 0 --prfc $p
-# 	done
-# done 
+for p in 90 92 95 97 99
+do
+	for k in 32 64 128 256
+	do
+		python compression.py --compression pruCWS --net original_nets/vgg19cifar100.h5 --dataset CIFAR100 --clusterfc $k --clustercnn $k --prfc $p
+	done
+done 
+
+for p in 90 92 95 97 99
+do
+	for k in 32 64 128 256
+	do
+		python compression.py --compression pruPWS --net original_nets/vgg19cifar100.h5 --dataset CIFAR100 --clusterfc $k --clustercnn $k --prfc $p
+	done
+done 
+
+for p in 90 92 95 97 99
+do
+	for k in 32 64 128 256
+	do
+		python compression.py --compression pruUQ --net original_nets/vgg19cifar100.h5 --dataset CIFAR100 --clusterfc $k --clustercnn $k --prfc $p
+	done
+done 
+
+
+
+
+for p in 90 92 95 97 99
+do
+	for k in 32 64 128 256
+	do
+		python compression.py --compression pruECSQ --net original_nets/vgg19cifar100.h5 --dataset CIFAR100 --clusterfc $k --clustercnn $k --prfc $p
+	done
+done 
