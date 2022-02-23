0300ae6866f2360c78144f616cdeb957ea4b5add
Giosuè Marinò <giosumarin@yahoo.com>
2022-02-23 19:58:29 +0100

    upd last run

--- experiments/time_space/runner_fievel.sh
+++ experiments/time_space/runner_fievel.sh
@@ -46,4 +46,4 @@
 # done 
 
 python uws_testing_space.py -t also_cnn -d ../performance_eval/VGG19/vgg19cifar100/pruECSQ_all/ -m ../performance_eval/VGG19/original_nets/vgg19cifar100.h5 -s cifar100 -q 0
-python uws_testing_space.py -t all -d ../performance_eval/VGG19/vgg19cifar100/uECSQ/ -m ../performance_eval/VGG19/original_nets/vgg19cifar100.h5 -s cifar100 -q 0
+#python uws_testing_space.py -t all -d ../performance_eval/VGG19/vgg19cifar100/uECSQ/ -m ../performance_eval/VGG19/original_nets/vgg19cifar100.h5 -s cifar100 -q 0
